04865c8d65c4482e701ec1d4bd0146cfe32fe6cd
Volha Kolas <olgakolos1411@gmail.com>
2017-01-17 12:21:58 +0300

    correct online prog

--- app/Http/Controllers/UserPageController.php
+++ app/Http/Controllers/UserPageController.php
@@ -10,7 +10,13 @@ use Illuminate\Support\Facades\DB;
 class UserPageController extends Controller
 {
     public function userpage() {
-        $names = User::select('name', 'surname', 'online')->where('id', '!=', auth()->id())->get();
+        $names = User::select('id', 'name', 'surname', 'online', 'time')->where('id', '!=', auth()->id())->get();
+        foreach ($names as $name) {
+            if((time() - $name->time)/60 > 15) {
+                User::where('id', $name->id)->update(['online' => 0]);
+            }
+        }
+        $names = User::select('name', 'surname', 'online', 'time')->where('id', '!=', auth()->id())->get();
 
 
         return view('holdem.userpage', compact('names'));
--- resources/views/holdem/userpage.blade.php
+++ resources/views/holdem/userpage.blade.php
@@ -2,15 +2,15 @@
 @extends('layouts.main')
 
 @section('content')
-   @foreach($names as $name)
-       {{$name->name . " " . $name->surname . " " . $name->online}}
-   @endforeach
+    @foreach($names as $name)
+        {{$name->name . " " . $name->surname . " " . $name->online}}
+    @endforeach
 
 
-   <form action="{{ route('pregame')  }}" method="post">
-       {{ csrf_field() }}
-       <button class="btn btn-primary">Рандомная игра</button>
-   </form>
+    <form action="{{ route('pregame')  }}" method="post">
+        {{ csrf_field() }}
+        <button class="btn btn-primary">Рандомная игра</button>
+    </form>
 
 
 @endsection
